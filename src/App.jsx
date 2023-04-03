e342f694119687e1e66540f5f58717597ce0fa33
hozlucas28 <hozlucas28@gmail.com>
2023-04-02 23:32:52 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { container, title, subTitle } from './App.module.css'
+
+function App() {
+	return (
+		<div className={container}>
+			<h1 className={title}>Template ViteJS</h1>
+			<h2 className={subTitle}>
+				<a
+					href='https://github.com/hozlucas28'
+					target='_blank'
+					rel='noreferrer'
+				>
+					@hozlucas28
+				</a>
+			</h2>
+		</div>
+	)
+}
+
+export default App
